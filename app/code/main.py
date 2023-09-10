ae3c5371ae74e4243b2123faa8f42f5b06acf7eb
Sunil Prajapati <sunilpzpt@gmail.com>
2023-09-10 17:20:50 +0700

    feat: multiple version pages for dash app

--- app/code/main.py
+++ app/code/main.py
@@ -20,7 +20,7 @@ scaler = pickle.load(open(scaler_path, 'rb'))
 
 # Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.CERULEAN]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 
 # Importing the brand names 
@@ -33,12 +33,23 @@ brand_options = [
     'Volvo'
 ]
 
-# App layout
-app.layout =html.Div([
+home_layout = html.Div([
     html.Div(children=[
         html.Div('Welcome to New Car Center', className="header"),
         html.Div("With our advance AI, Get the best price estimation for vehicle of your need!!!!", className="message"),
-        html.Strong("FILL ALL THE FIELDS FOR THE BEST RESULTS !!")
+        html.Div("Please click on the version below you wish to proceed with")
+    ]),
+
+    html.Div(children = [
+        dcc.Link('V1', className="version-link", href="/v1"),
+        dcc.Link('V2', className="version-link", href="/v2")
+    ], className="versions"),
+])
+
+v1_layout = html.Div([
+    html.Div(children=[
+        html.Div('Welcome to New Car Center', className="header"),
+        html.Div("With our advance AI, Get the best price estimation for vehicle of your need!!!!", className="message"),
     ]),
 
     html.Div(children=[
@@ -75,8 +86,62 @@ app.layout =html.Div([
     ], className="form"),
 
     html.Output(id="output-div", className="price-output"),
-], className="layout")
+])
 
+v2_layout = html.Div([
+    html.Div(children=[
+        html.Div('Welcome to New Car Center', className="header"),
+        html.Div("With our advance AI, Get the best price estimation for vehicle of your need!!!!", className="message"),
+    ]),
+
+    html.Div(children=[
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dbc.Label("Mileage (kmpl)"),
+                        dbc.Input(id="mileage-input", type="number", placeholder="Enter the mileage", required=False)
+
+                    ]
+                ),
+
+                html.Div(
+                    [
+                        dbc.Label("Engine (CC)"),
+                        dbc.Input(id="engine-input", type="number", placeholder="Enter the engine", required=False)
+                    ]
+                ),
+
+                html.Div(
+                    [
+                        dbc.Label("Max Power (bhp)"),
+                        dbc.Input(id="max-power-input", type="number", placeholder="Enter the maxpower", required=False)
+                    ]
+                ),
+            ],
+            className="form-fields"
+        ),
+
+        html.Output(id="error-div", className="error-message"),
+
+        dbc.Button(id="submit-button", children="Submit", color="primary", className="submit"),
+    ], className="form"),
+
+    html.Output(id="output-div", className="price-output"),
+])
+
+@app.callback(
+    Output('page-content', 'children'),
+    Input('url', 'pathname')
+)
+def display_page(pathname):
+    if pathname == '/v1':
+        return v1_layout
+    elif pathname == '/v2':
+        return v2_layout
+    else:
+        return home_layout
+    
 @callback(
     Output("error-div", "children"),
     Output("output-div", "children"),
@@ -104,6 +169,12 @@ def submit_form(n_clicks, mileage, max_power, engine):
 
     return '', f"Based on your input, the predicted selling price of such car is {predicted_selling_price} Baht"
 
+
+# App layout
+app.layout =html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+], className="layout")
 
 # Run the app
 if __name__ == '__main__':
